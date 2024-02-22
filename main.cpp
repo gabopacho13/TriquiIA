691ceff96fa47bf620b2513226c9b39bba9235d4
gabopacho13 <64044271+gabopacho13@users.noreply.github.com>
2024-02-21 22:13:07 -0500

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -75,16 +75,21 @@ void crearTablero(Grafo<Espacio>* tablero)
     }
 }
 
-void imprimirTablero(Grafo<Espacio>* tablero)
+int imprimirTablero(Grafo<Espacio>* tablero)
 {
+    int cont = 0;
     for (int i = 0; i < 3; i++)
     {
         for (int j = 0; j < 3; j++)
         {
-            cout << tablero->obtenerDato(j + (3*i)) << " ";
+            char caracter = tablero->obtenerDato(j + (3*i)).caracter;
+            cout << caracter << " ";
+            if (caracter == '_')
+                cont++;
         }
         cout << endl;
     }
+    return cont;
 }
 
 void alterarAristas (Grafo<Espacio>* tablero, int indice, double cambio)
@@ -100,26 +105,35 @@ void alterarAristas (Grafo<Espacio>* tablero, int indice, double cambio)
     }
 }
 
-void turnoMaquinaMin(Grafo<Espacio>* tablero, int indiceX, int sumaHeuristica)
+void turnoMaquinaMin(Grafo<Espacio>* tablero, int indiceX, int &sumaHeuristica)
 {
+    int minimo = 999;
     for (int i = 0; i < 9; i++)
     {
+        if (i == indiceX)
+            continue;
         if (tablero->obtenerDato(i).caracter == '_')
         {
+            int posMinimo = 0;
             alterarAristas(tablero, i, -4);
-            int sumaHeuristica = 0;
             for (int j = 0; j < 9; j++)
             {
                 if (tablero->obtenerDato(j).caracter == '_' || tablero->obtenerDato(j).caracter == 'O')
-                    sumaHeuristica += tablero->buscarArista(tablero->obtenerDato(i), tablero->obtenerDato(j));
+                {
+                    posMinimo += tablero->buscarArista(tablero->obtenerDato(i), tablero->obtenerDato(j));
+                }
             }
+            if (posMinimo < minimo)
+                minimo = posMinimo;
             alterarAristas(tablero, i, 4);
         }
     }
+    sumaHeuristica += minimo;
 }
 
 void turnoMaquinaMax(Grafo<Espacio>* tablero)
 {
+    cout << "Es mi turno!!" << endl;
     int maximo = -999;
     int indiceMejor = 1;
     for (int i = 0; i < 9; i++)
@@ -133,7 +147,7 @@ void turnoMaquinaMax(Grafo<Espacio>* tablero)
                 if (tablero->obtenerDato(j).caracter == '_' || tablero->obtenerDato(j).caracter == 'X')
                     sumaHeuristica += tablero->buscarArista(tablero->obtenerDato(i), tablero->obtenerDato(j));
             }
-
+            turnoMaquinaMin(tablero, i, sumaHeuristica);
             if (sumaHeuristica > maximo)
             {
                 maximo = sumaHeuristica;
@@ -158,31 +172,145 @@ void turnoMaquinaMax(Grafo<Espacio>* tablero)
     }
 }
 
+char verificarGanador(Grafo<Espacio>* tablero)
+{
+    int contDiagDer = 0;
+    int contDiagIzq = 0;
+    for (int i = 0; i < 3; i++)
+    {
+        int contHor = 0;
+        int contVert = 0;
+        for (int j = 0; j < 3; j++)
+        {
+            if (tablero->obtenerDato(i+3*j).caracter == 'O')
+                contVert--;
+            if (tablero->obtenerDato(3*i+j).caracter == 'O')
+                contHor--;
+            if (i == j && tablero->obtenerDato(2*i+2*j).caracter == 'O')
+                contDiagDer--;
+            if (i == 2-j && tablero->obtenerDato(3*i+j).caracter == 'O')
+                contDiagIzq--;
+            if (tablero->obtenerDato(i+3*j).caracter == 'X')
+                contVert++;
+            if (tablero->obtenerDato(3*i+j).caracter == 'X')
+                contHor++;
+            if (i == j && tablero->obtenerDato(2*i+2*j).caracter == 'X')
+                contDiagDer++;
+            if (i == 2-j && tablero->obtenerDato(3*i+j).caracter == 'X')
+                contDiagIzq++;
+        }
+        if (contVert == 3 || contHor == 3 || contDiagDer == 3 || contDiagIzq == 3)
+            return 'X';
+        if (contVert == -3 || contHor == -3 || contDiagDer == -3 || contDiagIzq == -3)
+            return 'O';
+        if (contVert == -2)
+        {
+            tablero->cambiarArista(tablero->obtenerDato(i), tablero->obtenerDato(i+3), -40);
+            tablero->cambiarArista(tablero->obtenerDato(i), tablero->obtenerDato(i+6), -40);
+            tablero->cambiarArista(tablero->obtenerDato(i+3), tablero->obtenerDato(i), -40);
+            tablero->cambiarArista(tablero->obtenerDato(i+6), tablero->obtenerDato(i), -40);
+        }
+        if (contHor == -2)
+        {
+            tablero->cambiarArista(tablero->obtenerDato(3*i), tablero->obtenerDato(3*i+1), -40);
+            tablero->cambiarArista(tablero->obtenerDato(3*i), tablero->obtenerDato(3*i+2), -40);
+            tablero->cambiarArista(tablero->obtenerDato(3*i+1), tablero->obtenerDato(3*i), -40);
+            tablero->cambiarArista(tablero->obtenerDato(3*i+2), tablero->obtenerDato(3*i), -40);
+        }
+        if (contVert == 2)
+        {
+            tablero->cambiarArista(tablero->obtenerDato(i), tablero->obtenerDato(i+3), 10);
+            tablero->cambiarArista(tablero->obtenerDato(i), tablero->obtenerDato(i+6), 10);
+            tablero->cambiarArista(tablero->obtenerDato(i+3), tablero->obtenerDato(i), 10);
+            tablero->cambiarArista(tablero->obtenerDato(i+6), tablero->obtenerDato(i), 10);
+        }
+        if (contHor == 2)
+        {
+            tablero->cambiarArista(tablero->obtenerDato(3*i), tablero->obtenerDato(3*i+1), 10);
+            tablero->cambiarArista(tablero->obtenerDato(3*i), tablero->obtenerDato(3*i+2), 10);
+            tablero->cambiarArista(tablero->obtenerDato(3*i+1), tablero->obtenerDato(3*i), 10);
+            tablero->cambiarArista(tablero->obtenerDato(3*i+2), tablero->obtenerDato(3*i), 10);
+        }
+    }
+    if (contDiagDer == -2)
+    {
+        tablero->cambiarArista(tablero->obtenerDato(0), tablero->obtenerDato(4), -40);
+        tablero->cambiarArista(tablero->obtenerDato(0), tablero->obtenerDato(8), -40);
+        tablero->cambiarArista(tablero->obtenerDato(4), tablero->obtenerDato(0), -40);
+        tablero->cambiarArista(tablero->obtenerDato(8), tablero->obtenerDato(0), -40);
+    }
+    if (contDiagIzq == -2)
+    {
+        tablero->cambiarArista(tablero->obtenerDato(2), tablero->obtenerDato(4), -40);
+        tablero->cambiarArista(tablero->obtenerDato(2), tablero->obtenerDato(6), -40);
+        tablero->cambiarArista(tablero->obtenerDato(6), tablero->obtenerDato(2), -40);
+        tablero->cambiarArista(tablero->obtenerDato(4), tablero->obtenerDato(2), -40);
+    }
+    if (contDiagDer == 2)
+    {
+        tablero->cambiarArista(tablero->obtenerDato(0), tablero->obtenerDato(4), 10);
+        tablero->cambiarArista(tablero->obtenerDato(0), tablero->obtenerDato(8), 10);
+        tablero->cambiarArista(tablero->obtenerDato(4), tablero->obtenerDato(0), 10);
+        tablero->cambiarArista(tablero->obtenerDato(8), tablero->obtenerDato(0), 10);
+    }
+    if (contDiagIzq == 2)
+    {
+        tablero->cambiarArista(tablero->obtenerDato(2), tablero->obtenerDato(4), 10);
+        tablero->cambiarArista(tablero->obtenerDato(2), tablero->obtenerDato(6), 10);
+        tablero->cambiarArista(tablero->obtenerDato(6), tablero->obtenerDato(2), 10);
+        tablero->cambiarArista(tablero->obtenerDato(4), tablero->obtenerDato(2), 10);
+    }
+    return '_';
+}
+
 void turnoJugador(Grafo<Espacio>* tablero)
 {
     int valor = 0;
     cout << "\nTu turno!! Escribe un numero de 1 a 9 para poner tu O" << endl;
-    cin >> valor;
-    Espacio aux;
-    aux.caracter = 'O';
-    aux.numEspacio = valor-1;
-    tablero->cambiarVertice(tablero->obtenerDato(valor-1), aux);
-    alterarAristas(tablero, valor-1, -4);
+    while (true){
+        cin >> valor;
+        if (tablero->obtenerDato(valor-1).caracter != '_'){
+            cout << "Ese ya esta ocupado! escoje un espacio vacio" << endl;
+            continue;
+        }
+        Espacio aux;
+        aux.caracter = 'O';
+        aux.numEspacio = valor-1;
+        tablero->cambiarVertice(tablero->obtenerDato(valor-1), aux);
+        alterarAristas(tablero, valor-1, -4);
+        break;
+    }
 }
 
 int main()
 {
     Grafo<Espacio>* tablero = new Grafo<Espacio>();
-    bool acabo = false;
+    char ganador = '_';
     crearTablero(tablero);
-    imprimirTablero(tablero);
     cout << "\nBienvenido al triqui! Juguemos. Yo sere las X y tu las O. Yo empiezo!" << endl;
-    while (!acabo)
+    while (true)
     {
         turnoMaquinaMax(tablero);
-        imprimirTablero(tablero);
+        if (imprimirTablero(tablero) == 0)
+            break;
+        cout << endl;
+        ganador = verificarGanador(tablero);
+        if (ganador != '_')
+            break;
         turnoJugador(tablero);
+        if (imprimirTablero(tablero) == 0)
+            break;
+        cout << endl;
+        ganador = verificarGanador(tablero);
+        if (verificarGanador(tablero) != '_')
+            break;
     }
+    if (ganador == 'X')
+        cout << "He ganado!" << endl;
+    else if (ganador == 'O')
+        cout << "Has ganado!" << endl;
+    else
+        cout << "Empatamos!" << endl;
     delete tablero;
     return 0;
 }
